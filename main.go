e924365ab78a0606b72709386e8e378704014dba
MaSayFl <masayganova@gmail.com>
2024-04-28 13:41:30 +0300

    fix double usage of http.ListenAndServe

--- main.go
+++ main.go
@@ -89,7 +89,6 @@ func main() {
 
     http.Handle("/metrics", promhttp.Handler())
     port := ":" + os.Getenv("metr1c_port") // Например 1599
-    http.ListenAndServe(port, nil)
     // Использую порт как 1c (i.e. 1545, 1540, 1541, 1560-1591)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
