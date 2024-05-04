a4d3c4d6bdc73c229f8f8fa23529e143fad2e5db
Alexander Gorelyshev <corvus-migratorius@proton.me>
2024-05-04 22:18:22 +0400

    Fix error handling

--- rac/rac.go
+++ rac/rac.go
@@ -1,17 +1,27 @@
 package rac
 
 import (
-	"fmt"
+	// "fmt"
 	"strings"
 )
 
-type ErrMalformedRACLine struct{}
+type ErrNoColonInRACLine struct{}
 
-func (e *ErrMalformedRACLine) Error() string {
-	return "could not split the RAC output line"
+func (e *ErrNoColonInRACLine) Error() string {
+	return "error: no colon in a RAC output line"
+}
+
+type ErrNoKeyInRACLine struct{}
+
+func (e *ErrNoKeyInRACLine) Error() string {
+	return "error: no key found in RAC output line"
 }
 
 func extractKeyValue(line string) (string, string, error) {
+	if !strings.Contains(line, ":") {
+		return "", "", &ErrNoColonInRACLine{}
+	}
+
 	data := strings.Split(line, ":")
 	key := strings.TrimSpace(data[0])
 	value := strings.TrimSpace(data[1])
@@ -19,8 +29,8 @@ func extractKeyValue(line string) (string, string, error) {
 	key = strings.ReplaceAll(key, `"`, "")
 	value = strings.ReplaceAll(value, `"`, "")
 
-	if key == "" || value == "" {
-		return key, value, &ErrMalformedRACLine{}
+	if key == "" {
+		return key, value, &ErrNoKeyInRACLine{}
 	}
 
 	return key, value, nil
--- rac/rac_test.go
+++ rac/rac_test.go
@@ -1,6 +1,7 @@
 package rac
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 )
@@ -9,7 +10,7 @@ func TestExtractKeyValueNormal(t *testing.T) {
 	input := "connection     : 3f97c035-b8e6-4b25-a72c-887b51a72b67"
 	expectedKey := "connection"
 	expectedValue := "3f97c035-b8e6-4b25-a72c-887b51a72b67"
-	
+
 	actualKey, actualValue, err := extractKeyValue(input)
 	if err != nil {
 		t.Error(err)
@@ -24,7 +25,7 @@ func TestExtractKeyValueQuotes(t *testing.T) {
 	input := `application     : "WebServerExtension"`
 	expectedKey := "application"
 	expectedValue := "WebServerExtension"
-	
+
 	actualKey, actualValue, err := extractKeyValue(input)
 	if err != nil {
 		t.Error(err)
@@ -32,6 +33,24 @@ func TestExtractKeyValueQuotes(t *testing.T) {
 
 	if expectedKey != actualKey || expectedValue != actualValue {
 		t.Fatalf("(actual) %#v: %#v != %#v: %#v (expected)\n", actualKey, actualValue, expectedKey, expectedValue)
+	}
+}
+
+func TestExtractKeyValueNoColon(t *testing.T) {
+	input := `application      "WebServerExtension"`
+
+	_, _, err := extractKeyValue(input)
+	if !errors.Is(err, &ErrNoColonInRACLine{}) {
+		t.Errorf("expected error not raised: %s", &ErrNoColonInRACLine{})
+	}
+}
+
+func TestExtractKeyValueNoKey(t *testing.T) {
+	input := `: "WebServerExtension"`
+
+	_, _, err := extractKeyValue(input)
+	if !errors.Is(err, &ErrNoKeyInRACLine{}) {
+		t.Errorf("expected error not raised: %s", &ErrNoKeyInRACLine{})
 	}
 }
 
